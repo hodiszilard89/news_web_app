ef351c5fef6bd3398d4b83ba854e3cb0aaaf37c3
hodiszilard89 <hodiszilard89@gmail.com>
2024-03-17 18:36:19 +0100

    03_17 mappák frissítése

--- client/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const SEARCH_CHANGE = "SEARCH_CHANGE";
-export const ADD_TODO = "ADD_TODO";
-export const DEL_TODO = "DEL_TODO";
-export const CHANGE_FLAG = "CHANGE_FLAG";
-export const UPDATE_TODO = "UPDATE_TODO";
-export const SET_USERS = "SET_USERS";
--- client/src/actions/search.js
+++
@@ -1,7 +0,0 @@
-import { SEARCH_CHANGE } from "./index";
-export function doSearchChange(newSearchValue) {
-  return {
-    type: SEARCH_CHANGE,
-    newSearchValue,
-  };
-}
